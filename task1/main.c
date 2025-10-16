7cee2f8bb84770bddda685f5dce609557f1ee487
dvrcky <kingkingzot@gmail.com>
2025-10-16 18:10:11 +0400

    last solution mb

--- task1/main.c
+++ task1/main.c
@@ -1,92 +1,40 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 
 typedef struct Result {
-	int string_count;
+	int line_count;
 	int word_count;
 	int symbol_count;
 } Result;
 
-FILE* make_buffer_for_stdin(void) {
-	FILE* file = fopen("temp.txt", "w+");
-
-	if (!file)
-		fprintf(stderr, "Something went wrong.");
-
-	char buffer[255];
-	while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-		fputs(buffer, file);
-	}
-
-	rewind(file);
-	return file;
-}
-
-int count_strings(FILE* file) {
-	int result = 0;
+Result count_file(FILE* file) {
+	Result result = {
+		.line_count = 0,
+		.word_count = 0,
+		.symbol_count = 0,
+	};
 
 	char temp;
-	while ((temp = fgetc(file)) != EOF) {
-		if (temp == '\n') ++result;
+	int count_words = 0, count_lines = 0, count_symbols = 0;
+	while ((temp = getc(file)) != EOF) {
+		if (temp == ' ' || temp == '\t' || temp == '\n' || temp == '\v') {
+			++count_words;
+		}
+
+		if (temp == '\n') {
+			++count_lines;
+		}
+
+		++count_symbols;
 	}
 
-	rewind(file);
-	return result;
-}
-
-int count_symbols(FILE* file) {
-	int result = 0;
-
-	char temp;
-	while ((temp = fgetc(file)) != EOF) ++result;
-
-	rewind(file);
-	return result;
-}
-
-int count_words(FILE* file) {
-	int result = 0;
-
-	char temp;
-	while ((temp = fgetc(file)) != EOF) {
-		if (temp == ' ' || temp == '\n' || temp == '\t' || temp == '\r' || temp == '\v') ++result;
-	}
-
-	rewind(file);
-	return result;
-}
-
-Result* count_from_file(FILE* file) {
-	bool to_remove = false;
-	if (file == stdin) {
-		file = make_buffer_for_stdin();
-		to_remove = true;
-	}
-
-	Result* result = (Result*)(malloc(sizeof(Result)));
-	if (!result)
-		fprintf(stderr, "Something went wrong.");
-
-	if (!file) {
-		result->string_count = 0;
-		result->symbol_count = 0;
-		result->word_count = 0;
-	}
-
-	result->symbol_count = count_symbols(file);
-	result->string_count = count_strings(file);
-	result->word_count = count_words(file);
-
-	if (to_remove) unlink("temp.txt");
+	result.line_count = count_lines;
+	result.symbol_count = count_symbols;
+	result.word_count = count_words;
 
 	return result;
-}
-
-void clean_result(Result* result) {
-	free(result);
 }
 
 int main(int argc, char** argv) {
@@ -95,16 +43,15 @@ int main(int argc, char** argv) {
 	if (argc > 1) {
 		file = fopen(argv[1], "r");
 		if (!file) {
-			fprintf(stderr, "Something went wrong.");
+			perror("Couldn`t open file.");
+			return 1;
 		}
 	} else {
 		file = stdin;
 	}
 
-	Result* result = count_from_file(file);
-	printf("%i\n%i\n%i\n", result->string_count, result->word_count, result->symbol_count);
-
-	clean_result(result);
+	Result result = count_file(file);
+	printf("%i\n%i\n%i\n", result.line_count, result.word_count, result.symbol_count);
 	fclose(file);
 
 	return 0;
